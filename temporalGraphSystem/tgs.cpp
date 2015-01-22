ff2301b3ec9376306f1d58f2f776781fb9f3c597
master <master>
2015-01-22 17:26:18 +0800

    topological level done

--- temporalGraphSystem/tgs.cpp
+++ temporalGraphSystem/tgs.cpp
@@ -4,9 +4,11 @@
 #include <algorithm>
 #include <set>
 #include <map>
+#include <queue>
 #include <assert.h>
-//string in_path = "/yuzhen/toyP";
-string in_path = "/yuzhen/sociopatterns";
+string in_path = "/yuzhen/toyP";
+//string in_path = "/yuzhen/sociopatterns";
+//string in_path = "/yuzhen/editP";
 string out_path = "/yuzhen/output";
 bool use_combiner = true;
 
@@ -51,6 +53,17 @@ public:
 	int countIn;
 	vector<int> Vin;
 	vector<vector<vw> > VinNeighbors;
+	
+	
+	vector<int> toOut;
+	vector<int> toIn;
+	//topological level
+	vector<int> topologicalLevelIn;
+	vector<int> topologicalLevelOut;
+	vector<int> inDegreeIn;
+	vector<int> inDegreeOut;
+	vector<int> countinDegreeIn;
+	vector<int> countinDegreeOut;
 
 	//Step 5.1: define UDF1: query -> vertex's query-specific init state
     virtual int init_value(vector<int>& query) //QValueT init_value(QueryT& query
@@ -111,86 +124,262 @@ public:
 			for (int j = 0; j < VoutNeighbors[i].size(); ++ j) {tmp = VoutNeighbors[i][j].v; tmp = VinNeighbors[i][j].w;}
 		}
     }
-    virtual void preCompute(MessageContainer& messages)
+    virtual void preCompute(MessageContainer& messages, int phaseNum)
     {
-    	if (superstep() == 1)
+    	if (phaseNum == 1)
     	{
-    		//build out-neighbors
-    		
-    		countOut = 0;
-    		for (int i = 0; i < nqvalue().neighbors.size(); ++ i)
-    		{
-    			for (int j = 0; j < nqvalue().tw[i].size(); ++ j)
-    			{
-    				if (mOut.count(nqvalue().tw[i][j].first) == 0) 
-    				{
-    					mOut[nqvalue().tw[i][j].first] = 1;
-    				}
-    			}
-    		}
-    		for (std::map<int,int>::iterator it = mOut.begin(); it != mOut.end(); it ++)
-    		{
-    			it->second = countOut ++;
-    			Vout.push_back(it->first);
-    		}
-    		assert(countOut == Vout.size());
-    		VoutNeighbors.resize(countOut);
-    		for (int i = 0; i < nqvalue().neighbors.size(); ++ i)
-    		{
-    			for (int j = 0; j < nqvalue().tw[i].size(); ++ j)
-    			{
-    				VoutNeighbors[mOut[nqvalue().tw[i][j].first] ].push_back(vw{nqvalue().neighbors[i], nqvalue().tw[i][j].second});		
-    			}
-    		}
-    		//out-neighbors built done
-    		
-    		
-    		
-    		//send messages
-    		//message: id, t+w, w
-    		vector<int> send(3);
-    		for (int i = 0; i < nqvalue().neighbors.size(); ++ i)
-    		{
-    			//send_message(nqvalue().neighbors[i], id);
-    			send[0]=id;
-    			for (int j = 0; j < nqvalue().tw[i].size(); ++ j)
-    			{
-    				send[1]=nqvalue().tw[i][j].first+nqvalue().tw[i][j].second;
-    				send[2]=nqvalue().tw[i][j].second;
-    				send_message(nqvalue().neighbors[i], send);
-    			}
-    		}
-    		//send done
-    	}
-    	else
-    	{
-    		
-    		//build in-neighbors
-    		countIn = 0;
-    		for (int i = 0; i < messages.size(); ++ i)
-    		{
-    			vector<int>& edge = messages[i];
-    			if (mIn.count(edge[1]) == 0) mIn[edge[1]] = 1;
-    		}
-    		for (std::map<int,int>::iterator it = mIn.begin();it!=mIn.end();it++)
-    		{
-    			it->second = countIn ++;
-    			Vin.push_back(it->first);
-    		}
-    		assert(countIn == Vin.size());
-    		VinNeighbors.resize(countIn);
-    		for (int i = 0; i < messages.size(); ++ i)
-    		{
-    			vector<int>& edge = messages[i];
-    			VinNeighbors[mIn[edge[1]] ].push_back(vw{edge[0],edge[2]});
-    		}
-    		//in-neighbors built done
-    		
-    		//build index done
-    		printTransformInfo();
-    		
-    	}
-    	vote_to_halt();
+			if (superstep() == 1) //build neighbors
+			{
+				//build out-neighbors
+				countOut = 0;
+				for (int i = 0; i < nqvalue().neighbors.size(); ++ i)
+				{
+					for (int j = 0; j < nqvalue().tw[i].size(); ++ j)
+					{
+						if (mOut.count(nqvalue().tw[i][j].first) == 0) 
+						{
+							mOut[nqvalue().tw[i][j].first] = 1;
+						}
+					}
+				}
+				for (std::map<int,int>::iterator it = mOut.begin(); it != mOut.end(); it ++)
+				{
+					it->second = countOut ++;
+					Vout.push_back(it->first);
+				}
+				assert(countOut == Vout.size());
+				VoutNeighbors.resize(countOut);
+				for (int i = 0; i < nqvalue().neighbors.size(); ++ i)
+				{
+					for (int j = 0; j < nqvalue().tw[i].size(); ++ j)
+					{
+						VoutNeighbors[mOut[nqvalue().tw[i][j].first] ].push_back(vw{nqvalue().neighbors[i], nqvalue().tw[i][j].second});		
+					}
+				}
+				//out-neighbors built done
+				
+				
+				
+				//send messages
+				//message: id, t+w, w
+				vector<int> send(3);
+				for (int i = 0; i < nqvalue().neighbors.size(); ++ i)
+				{
+					//send_message(nqvalue().neighbors[i], id);
+					send[0]=id;
+					for (int j = 0; j < nqvalue().tw[i].size(); ++ j)
+					{
+						send[1]=nqvalue().tw[i][j].first+nqvalue().tw[i][j].second;
+						send[2]=nqvalue().tw[i][j].second;
+						send_message(nqvalue().neighbors[i], send);
+					}
+				}
+				//send done
+				
+				//release memory
+				for (int i = 0; i < nqvalue().neighbors.size(); ++ i) vector<pair<int,int> >().swap(nqvalue().tw[i]);
+				vector<int>().swap(nqvalue().neighbors);
+				vector<vector<pair<int,int> > >().swap(nqvalue().tw);
+			}
+			else if (superstep() == 2)
+			{
+				
+				//build in-neighbors
+				countIn = 0;
+				for (int i = 0; i < messages.size(); ++ i)
+				{
+					vector<int>& edge = messages[i];
+					if (mIn.count(edge[1]) == 0) mIn[edge[1]] = 1;
+				}
+				for (std::map<int,int>::iterator it = mIn.begin();it!=mIn.end();it++)
+				{
+					it->second = countIn ++;
+					Vin.push_back(it->first);
+				}
+				assert(countIn == Vin.size());
+				VinNeighbors.resize(countIn);
+				for (int i = 0; i < messages.size(); ++ i)
+				{
+					vector<int>& edge = messages[i];
+					VinNeighbors[mIn[edge[1]] ].push_back(vw{edge[0],edge[2]});
+				}
+				//in-neighbors built done
+				
+				
+				//build toIn, toOut
+				toIn.resize(Vout.size(),-1);
+				toOut.resize(Vin.size(),-1);
+				
+				int lastOut = -1;
+				for (int i = Vin.size()-1; i >= 0; --i)
+				{
+					std::map<int,int>::iterator it;
+					it = mOut.lower_bound(Vin[i]);
+					if (it!=mOut.end() && it->second != lastOut)
+					{
+						lastOut = it->second;
+						toOut[i] = it->second;
+						toIn[it->second] = i;
+					}
+				}
+				
+				//cout << "id:" << id << endl;
+				//initialize inDegree 
+				inDegreeIn.resize(Vin.size());
+				inDegreeOut.resize(Vout.size());
+				for (int i = 0; i < Vin.size(); ++ i)
+				{
+					inDegreeIn[i] = VinNeighbors[i].size()+(i==0?0:1);
+					//cout << inDegreeIn[i] << endl;
+				}
+				for (int i = 0; i < Vout.size(); ++ i)
+				{
+					inDegreeOut[i] = (toIn[i]==-1?0:1) + (i==0?0:1);
+					//cout << inDegreeOut[i] << endl;
+				}
+				
+				//build neighbors done
+				printTransformInfo();
+				
+				
+				vote_to_halt();
+			}
+			
+		}
+		else if (phaseNum == 2) //build topological level
+		{
+			//cout << "phaseNum: " << phaseNum << endl;
+			if (superstep() == 1)
+			{
+				
+				countinDegreeIn.resize(Vin.size(), 0);
+				countinDegreeOut.resize(Vout.size(), 0);
+				topologicalLevelIn.resize(Vin.size(), -1);
+				topologicalLevelOut.resize(Vout.size(), -1);
+				
+				vector<int> send(2); //message: level, arrivalTime, 
+				if (Vout.size() > 0 && inDegreeOut[0] == 0)
+				{
+					topologicalLevelOut[0] = 0;
+					send[0] = 1;
+					for (int j = 0; j < VoutNeighbors[0].size(); ++ j)
+					{
+						send[1] = Vout[0] + VoutNeighbors[0][j].w;
+						send_message(VoutNeighbors[0][j].v, send);
+					}
+					
+					//check other Vout
+					int nxt_id = 1;
+					while(nxt_id < Vout.size())
+					{
+						countinDegreeOut[nxt_id] ++;
+						if (countinDegreeOut[nxt_id] == inDegreeOut[nxt_id])
+						{
+							topologicalLevelOut[nxt_id] = topologicalLevelOut[nxt_id-1] + 1;
+							send[0] = topologicalLevelOut[nxt_id]+1;
+							for (int j = 0; j < VoutNeighbors[nxt_id].size(); ++ j)
+							{
+								send[1] = Vout[nxt_id] + VoutNeighbors[nxt_id][j].w;
+								send_message(VoutNeighbors[nxt_id][j].v, send);
+							}
+							nxt_id++;
+						}
+						else
+						{
+							break;
+						}
+					}
+				}
+			}
+			else
+			{
+				queue<pair<int,bool> > q; //id, 0/1,    0 for Vin, 1 for Vout
+				for (int i = 0; i < messages.size(); ++ i)
+				{
+					vector<int>& inMsg = messages[i];
+					int in_index = mIn[inMsg[1]];
+					countinDegreeIn[in_index] ++;
+					
+					topologicalLevelIn[in_index] = max(topologicalLevelIn[in_index],inMsg[0]);
+					if (countinDegreeIn[in_index] == inDegreeIn[in_index])
+					{
+						
+						q.push(make_pair(in_index, 0));
+					}
+				}
+				while(!q.empty())
+				{
+					pair<int,bool> cur = q.front(); q.pop();
+					if (cur.second == 0) //Vin
+					{
+						if (cur.first != Vin.size()-1)
+						{
+							int tmp = cur.first+1;
+							countinDegreeIn[tmp] ++;
+							if (countinDegreeIn[tmp] == inDegreeIn[tmp])
+							{
+								topologicalLevelIn[tmp] = max(topologicalLevelIn[tmp],topologicalLevelIn[cur.first]+1);
+								q.push(make_pair(tmp, 0));
+							}
+						}
+						int tmp = toOut[cur.first];
+						if (tmp != -1)
+						{
+							countinDegreeOut[tmp] ++;
+							if (countinDegreeOut[tmp] == inDegreeOut[tmp])
+							{
+								topologicalLevelOut[tmp] = max(topologicalLevelOut[tmp],topologicalLevelIn[cur.first]+1);
+								q.push(make_pair(tmp,1));
+							}
+						}
+					}
+					else if (cur.second == 1) //Vout
+					{
+						if (cur.first != Vout.size()-1)
+						{
+							int tmp = cur.first+1;
+							countinDegreeOut[tmp]++;
+							if (countinDegreeOut[tmp] == inDegreeOut[tmp])
+							{
+								topologicalLevelOut[tmp] = max(topologicalLevelOut[tmp],topologicalLevelOut[cur.first]+1);
+								q.push(make_pair(tmp,1));
+							}
+						}
+						vector<int> send(2); //level, arrivalTime
+						send[0] = topologicalLevelOut[cur.first]+1;
+						for (int i = 0; i < VoutNeighbors[cur.first].size(); ++ i)
+						{
+							send[1] = Vout[cur.first]+VoutNeighbors[cur.first][i].w;
+							send_message(VoutNeighbors[cur.first][i].v, send);
+						}
+					}
+				}
+			}
+			vote_to_halt();
+		}
+		else if (phaseNum == 3)
+		{
+		
+			//release memory
+			vector<int>().swap(countinDegreeIn);
+			vector<int>().swap(countinDegreeOut);
+			vector<int>().swap(inDegreeIn);
+			vector<int>().swap(inDegreeOut);
+			
+			
+			//cout << "phaseNum: " << phaseNum << endl;
+			/*
+			cout << "id: " << id << endl;
+			for (int i = 0; i < Vin.size(); ++ i)
+			{
+				cout << topologicalLevelIn[i] << endl;
+			}
+			for (int i = 0; i < Vout.size(); ++ i)
+			{
+				cout << topologicalLevelOut[i] << endl;
+			}
+			*/
+		}
     }
     
     
@@ -250,6 +439,8 @@ public:
 
 int main(int argc, char* argv[])
 {
+	in_path = argv[1];
+	
 	WorkerParams param;
     param.input_path = in_path;
     param.output_path = out_path;
